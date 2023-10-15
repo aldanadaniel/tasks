4ff893d150a5c3455ab983b42dcb2527759becb9
aldana daniel <aldanadaniel162@gmail.com>
2023-10-14 19:33:04 -0500

    cors config

--- src/app.js
+++ src/app.js
@@ -15,7 +15,14 @@ app.set("port", 3000);
 
 app.use(cors({
     origin: 'https://localhost:4004',
+    optionsSuccessStatus: 200, // For legacy browser support
+    allowedHeaders: 'access-control-expose-headers, Access-Control-Allow-Credentials, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Content-Type, Sap-Messages, sap-messages, location, set-cookie, X-Forwarded-Proto, x-forwarded-proto, X-auth, x-csrf-token, Authorization',
+    exposedHeaders: 'Content-Type, Sap-Messages, sap-messages',
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
 }));
+
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
